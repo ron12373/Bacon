f8da5e6ada74d65512137006b0acc375b14dca26
BaconButPro <147504175+ron12373@users.noreply.github.com>
2024-10-18 17:27:38 +0700

    Update BaconButPro.user.js

--- Userscript-Free/BaconButPro.user.js
+++ Userscript-Free/BaconButPro.user.js
@@ -53,6 +53,6 @@
 // @connect      hohohubv-ac90f67762c4.herokuapp.com
 // @connect      tsuo-script.xyz
 // @icon         https://cdn141.picsart.com/351217840073211.png
-// @downloadURL https://github.com/ron12373/Userscript-free/raw/main/BaconButPro.user.js
-// @updateURL https://github.com/ron12373/Userscript-free/raw/main/BaconButPro.user.js
+// @downloadURL https://github.com/ron12373/Bacon/raw/main/Userscript-Free/BaconButPro.user.js
+// @updateURL https://github.com/ron12373/Bacon/raw/main/Userscript-Free/BaconButPro.user.js
 // ==/UserScript==
