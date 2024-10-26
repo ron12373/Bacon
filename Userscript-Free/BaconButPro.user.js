6d9d4ab128e2f83a3be15be68aacf7fd6965d6a4
BaconButPro <147504175+ron12373@users.noreply.github.com>
2024-10-26 16:23:27 +0700

    Update BaconButPro.user.js

--- Userscript-Free/BaconButPro.user.js
+++ Userscript-Free/BaconButPro.user.js
@@ -2,7 +2,7 @@
 // @name         Bypass Key System
 // @homepageURL  https://discord.gg/gFNAH7WNZj
 // @namespace    https://greasyfork.org/en/scripts/493960-arceus-bypass
-// @version      1.9.9
+// @version      2.0
 // @description  Bypass Arceus, Hydrogen, Delta, Trigon, Vegax, Codex, Linkvertise, panda, etc,...
 // @author       Bacon But Pro
 // @match        https://linkvertise.com/376138/arceus-x-neo-key-system-1?o=sharing*
@@ -29,6 +29,7 @@
 // @match        https://sub2unlock.com/*
 // @match        https://*.sub2get.com/*
 // @match        https://boost.ink/*
+// @match        https://trigonevo.fun/whitelist/?HWID=*
 // @require     https://greasyfork.org/scripts/464929-module-jquery-xiaoying/code/module_jquery_XiaoYing.js
 // @require     https://greasyfork.org/scripts/464780-global-module/code/global_module.js
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js
