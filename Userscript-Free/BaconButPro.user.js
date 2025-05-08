53ebcba45848e7480995b6130652702fa232f2c3
BaconButPro <147504175+ron12373@users.noreply.github.com>
2025-05-08 17:03:09 +0700

    Update BaconButPro.user.js

--- Userscript-Free/BaconButPro.user.js
+++ Userscript-Free/BaconButPro.user.js
@@ -2,13 +2,13 @@
 // @name         Bypass Key System
 // @homepageURL  https://discord.gg/gFNAH7WNZj
 // @namespace    https://tampermonkey.net/
-// @version      2.0.3
+// @version      2.0.4
 // @description  Bypass Arceus, Hydrogen, Delta, Trigon, Vegax, Codex, Linkvertise, panda, etc,...
 // @author       Bacon But Pro
 // @match        https://linkvertise.com/376138/arceus-x-neo-key-system-1?o=sharing*
 // @match        https://linkvertise.com/*/*
 // @match        https://rekonise.com/*
-// @match        https://mboost.me/a/*
+// @match        https://mboost.me/*
 // @match        https://loot-link.com/s?*
 // @match        https://loot-links.com/s?*
 // @match        https://lootlink.org/s?*
@@ -21,32 +21,36 @@
 // @match        https://links-loot.com/s?*
 // @match        https://linksloot.net/s?*
 // @match        https://spdmteam.com/key-system*
-// @match       https://loot-link.com/s?*
-// @match       https://loot-links.com/s?*
-// @match       https://lootlink.org/s?*
-// @match       https://lootlinks.co/s?*
-// @match       https://lootdest.info/s?*
-// @match       https://lootdest.org/s?*
-// @match       https://lootdest.com/s?*
-// @match       https://links-loot.com/s?*
-// @match       https://linksloot.net/s?*
-// @match       https://flux.li/android/external/start.php?HWID=*
-// @require     https://greasyfork.org/scripts/464929-module-jquery-xiaoying/code/module_jquery_XiaoYing.js
-// @require     https://greasyfork.org/scripts/464780-global-module/code/global_module.js
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js
-// @require     https://github.com/ron12373/Bacon/raw/main/Userscript-Free/Main.js
+// @match        https://loot-link.com/s?*
+// @match        https://loot-links.com/s?*
+// @match        https://lootlink.org/s?*
+// @match        https://lootlinks.co/s?*
+// @match        https://lootdest.info/s?*
+// @match        https://lootdest.org/s?*
+// @match        https://lootdest.com/s?*
+// @match        https://links-loot.com/s?*
+// @match        https://linksloot.net/s?*
+// @match        https://flux.li/android/external/start.php?HWID=*
+// @match        https://auth.platoboost.com/*
+// @match        https://auth.platoboost.net/*
+// @match        https://auth.platorelay.com/*
+// @match        https://krnl.cat/*
+// @require      https://greasyfork.org/scripts/464929-module-jquery-xiaoying/code/module_jquery_XiaoYing.js
+// @require      https://greasyfork.org/scripts/464780-global-module/code/global_module.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js
+// @require      https://cdn.rawgit.com/ricmoo/aes-js/e27b99df/index.js
+// @require      https://github.com/ron12373/Bacon/raw/main/Userscript-Free/Main.js
 
 // @run-at       document-end
 // @grant        GM_xmlhttpRequest
 // @grant        GM_notification
 // @grant        GM_openInTab
+// @grant        GM_addElement
 
 // @license      Bacon
 // @connect      linkvertise.com
-// @connect      short-jambo.com
 // @connect      api.codex.lol
 // @connect      cdn.jsdelivr.net
-// @connect      fluxteam.net
 // @connect      spdmteam.com
 // @connect      api.keyrblx.com
 // @connect      pandadevelopment.net
